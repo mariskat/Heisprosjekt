ba59238505ea2c80277276833631ef8319f22ff5
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 11:13:00 +0100

    Update elevator.h

--- elevator.h
+++ elevator.h
@@ -1,1 +1,13 @@
 
+//Variable
+bool previous_dir;
+bool previous_floor;
+bool door_open;
+
+
+//Funksjoner
+int arrived_at_floor();
+void motor_start();
+void motor_stop();
+void set_direction();
+void get_current_dir();
