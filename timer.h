9d88e8325c8f0328bc12e075600f851f8d97dcaf
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 15:03:50 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -4,11 +4,22 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <time.h>
 
 //Variable: 
-int timer_ value;
+static clock_t start_time = -1;                 //Static - da bevarer den verdien ute av scope. Døren er i utgangspunktet lukket: -1. 
 
 //Funksjoner:
-void timer_start();
-void timer_stop();
-bool timer_timeIsUp();
+
+void timer_start(void);
+
+void timer_stop(void);
+
+bool timer_timeIsUp(void);
+
+
+
+
+
+
+#endif /*timer.h*/
