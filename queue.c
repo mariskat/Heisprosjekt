9d36714f3d7de9f46642fca44d960c51a8365698
laksiya <36662731+laksiya@users.noreply.github.com>
2018-02-20 14:21:10 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -1,11 +1,40 @@
+#include "queue.h"
+#include "elev.h"
+#include <assert.h>
+#include <stdbool.h>
+#include <time.h>
+#include <stdio.h>
 
 
 
-int orders[][];
+int orders[4][3]={
+  {0,0,0},
+  {0,0,0},
+  {0,0,0},
+  {0,0,0}
+};
 
 //Funksjoner:
-void add_to_queue(floor,type);
-bool check_floor_orders();
-void clear_orders_at_floor();
-void clear_all_orders();
-bool get_new_direction();
+
+//Given floor and buttontype, this function sets the following button to 1 in queue.
+void add_to_queue(int floor, elev_button_type_t button){
+  printf("Running add_to_queue");
+  orders[floor][button]={1};
+};    
+
+//Sums the orders of the floor, returns 0 if no orders, 1 if else.
+bool check_floor_orders(int floor){
+  printf("Running check_floor_orders");
+  return (orders[floor][0]+orders[floor][1]+orders[floor][2]);
+};
+
+//Clears floor orders
+void clear_orders_at_floor(int floor){
+  printf("Running clear_orders_at_floor");
+  orders[floor]={0,0,0};
+  printf("Done clear_orders_at_floor");
+};
+
+bool get_new_direction(){
+  TILSTANDSMASKIN????
+};
