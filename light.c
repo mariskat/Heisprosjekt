378a402cc070aa1c2857d9c36486ac4509270f54
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 13:14:12 +0100

    Update light.c

--- light.c
+++ light.c
@@ -1,13 +1,26 @@
 #include "elev.h"
 #include "io.h"
 
+//FUNKSJONER FRA ELEV.C: 
 
-//Funksjoner
-void set_lights( floor,type);
-void clear_lights_at_floor();
-void door_open();
-void door_close();
-void set_floor_lights(floor);
-void floor_indicator();
+//Door open or closed (1: open, 0: closed)
+//void elev_set_door_open_lamp(int value);
+
+//Stoplamp on or off (1:on, 0: off)
+//void elev_set_stop_lamp(int value);
+
+//Set buttonlight
+//void elev_set_button_lamp(elev_button_type_t button, int floor, int value);
+
+//Floor indicator
+//void elev_set_floor_indicator(int floor);
 
 
+
+void clear_lights_at_floor(){
+  //Implementeres vha trippel forløkke elev_set_button_lamp(a,b,0);
+
+
+
+
+  
