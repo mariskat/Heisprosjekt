cc8e16e908586a5c6494411a5659757698de74b3
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 12:54:04 +0100

    Update light.c

--- light.c
+++ light.c
@@ -1,1 +1,13 @@
+#include "elev.h"
+#include "io.h"
+
+
+//Funksjoner
+void set_lights(floor,type);
+void clear_lights_at_floor;
+void door_open();
+void door_close();
+void set_floor_lights(floor);
+void floor_indicator();
+
 
