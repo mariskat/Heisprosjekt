aac892b3a7ead3f0d346d156121c13c1ecf2091b
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 11:17:19 +0100

    Update queue.h

--- queue.h
+++ queue.h
@@ -1,1 +1,11 @@
 
+
+//Variable: 
+int orders[][];
+
+//Funksjoner:
+void add_to_queue(floor,type);
+bool check_floor_orders();
+void clear_orders_at_floor();
+void clear_all_orders();
+bool get_new_direction();
