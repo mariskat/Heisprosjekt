023c520030c63b40d33309d6009b8822733acacd
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-20 11:15:19 +0100

    Update light.h

--- light.h
+++ light.h
@@ -1,1 +1,10 @@
 
+#include timer.h
+
+//Funksjoner
+void set_lights(floor,type);
+void clear_lights_at_floor;
+void door_open();
+void door_close();
+void set_floor_lights(floor);
+void floor_indicator();
