dc6577b4a112bbd577209850ebe34e44a3b0f914
laksiya <36662731+laksiya@users.noreply.github.com>
2018-02-21 15:05:41 +0100

    Update fsm.h

--- fsm.h
+++ fsm.h
@@ -1,4 +1,5 @@
-
-
-void clear_lights_at_floor(floor);
-
+void fsm_event_stop_pressed();
+void fsm_event_stop_released();
+void fsm_event_order_button_pressed();
+void fsm_event_arrived_at_floor(int new_floor);
+void fsm_event_door_closed();
