d7661c5b1eabe860fa8c15d7745750ba2618f0c8
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 13:25:36 +0100

    Update elevator.h

--- elevator.h
+++ elevator.h
@@ -6,20 +6,14 @@
 #include <assert.h>
 #include <stdlib.h>
 
- 
-//Variable
-bool previous_dir;
-bool previous_floor;
-bool door_open;
+/*Included from elev.h:
 
+Sets the motor direction of the elevator: elev_motor_direction_t is enum: DIRN_DOWN=-1, DIRN_STOP=0, DIRN_UP=1
+void elev_set_motor_direction(elev_motor_direction_t dirn);
 
-//Funksjoner
-int arrived_at_floor();
-void motor_start();
-void motor_stop();
-void set_direction();
-void get_current_dir();
-
+Get floor sensor signal: Not on floor = -1, On floor = 0-3 (ground fl.- top fl.)
+int elev_get_floor_sensor_signal(void);
+*/
 
 //elev_button_type_t er en enum: BUTTON_CALL_UP=0, BUTTON_CALL_DOWN=1, BUTTON_COMMAND=2
 int elev_get_button_signal(elev_button_type_t button, int floor);
