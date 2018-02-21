43c1247f7e2b0ceee45802ea340e11571f200ab4
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 13:47:46 +0100

    Delete elevator.c

--- elevator.c
+++
@@ -1,8 +0,0 @@
-
-int arrived_at_floor();
-void motor_start();
-void motor_stop();
-void set_direction();
-void get_current_dir();
-
-int elev_get_button_signal(elev_button_type_t button, int floor);
