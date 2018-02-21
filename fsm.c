a1aa6735f535dcb52c19d30d55dae1e0c685603a
laksiya <36662731+laksiya@users.noreply.github.com>
2018-02-21 14:58:58 +0100

    Update fsm.c

--- fsm.c
+++ fsm.c
@@ -5,15 +5,6 @@
 
 #include <stdio.h>
 
-/*Included from elev.h:
-
-Sets the motor direction of the elevator: elev_motor_direction_t is enum: DIRN_DOWN=-1, DIRN_STOP=0, DIRN_UP=1
-void elev_set_motor_direction(elev_motor_direction_t dirn);
-
-Get floor sensor signal: Not on floor = -1, On floor = 0-3 (ground fl.- top fl.)
-int elev_get_floor_sensor_signal(void);
-*/
-
 typedef enum{
 	INITIALIZE,
 	IDLE,
@@ -24,4 +15,168 @@ typedef enum{
 } state_t;
 
 state_t currentstate = INITIALIZE;
+int currentFloor;
+elev_motor_direction_t direction;
 
+//S
+void fsm_event_arrived_at_floor(int new_floor){
+	currentfloor = new_floor;
+	
+	switch(currentstate){
+			
+	case INITIALIZE:
+		elev_set_motor_direction(DIRN_STOP);
+		elev_set_floor_indicator(currentfloor);
+		currentstate = IDLE;
+		break;
+
+	case IDLE:		
+	case DOOROPEN:
+		elev_set_door_open_lamp(1);
+		clear_orders_at_floor(currentfloor);
+		timer_start();
+
+			for(int button = 0; button < N_BUTTONS; button++){
+                	elev_set_button_lamp(button,currentfloor,0);
+	        }
+		break;
+
+	case MOVING:
+		elev_set_floor_indicator(currentfloor);
+
+		if(queue_should_stop(currentfloor, direction)){
+			elev_set_motor_direction(DIRN_STOP);
+			elev_set_door_open_lamp(1);
+			clear_orders_at_floor(currentfloor);
+			timer_start();
+			currentstate = DOOROPEN;
+
+			for(button = 0; button < N_BUTTONS; button++){
+                	elev_set_button_lamp(button,currentfloor,0);
+	        	}
+		
+		}
+		break;
+
+	default:
+		break;
+	}
+}
+
+
+void fsm_event_order_button_pressed(button, floor){ 
+	switch(currentstate){
+        case IDLE:
+
+        	if (floor!=currentfloor){
+				add_to_queue(button, floor);
+				elev_set_button_lamp(button, floor, 1);
+				direction = queue_get_direction(previous_direction, currentfloor);
+				elev_set_motor_direction(direction);
+				previous_direction = direction;
+				currentstate = MOVING;
+		}
+		else{
+				elev_set_door_open_lamp(1);
+				timer_start();
+				currentstate = DOOROPEN;
+			
+		} 
+		break;
+			
+	case DOOROPEN:
+	case MOVING:
+		queue_add_orders(button, floor);
+		elev_set_button_lamp(button, floor, 1);
+
+		break;
+
+	case STOPBETWEENFLOORS:
+		queue_add_orders(button, floor);
+
+		if (floor==currentfloor){
+			elev_set_motor_direction(-(previous_direction));
+		} 
+		else {
+			direction = queue_get_direction(previous_direction, currentfloor);
+			elev_set_motor_direction(direction);
+		}
+		currentstate = MOVING;
+		break;
+
+	default:
+        	break;
+	}
+	
+	void fsm_event_stop_pressed(){ 
+	switch(currentstate){
+			
+	case MOVING:
+	case DOOROPEN:
+	case STOPBETWEENFLOORS:
+	case IDLE:
+		elev_set_motor_direction(DIRN_STOP);
+		queue_remove_all_orders();
+		for(floorbtn = 0; floorbtn < N_FLOORS; floorbtn++){
+           		for(button = 0; button < N_BUTTONS; button++){
+                		elev_set_button_lamp(button,floorbtn,0);
+          		  }
+        		}
+		elev_set_stop_lamp(1);
+
+		if (elev_get_floor_sensor_signal() != -1){
+			elev_set_door_open_lamp(1);
+			timer_start();
+		}
+
+		currentstate = EMERGENCYSTOP;
+		break;
+
+	default:
+		break;
+	}
+}
+
+void fsm_event_stop_released(){
+	switch(currentstate){
+	case EMERGENCYSTOP:
+		elev_set_stop_lamp(0);
+		
+		if (elev_get_floor_sensor_signal() == -1){
+		    currentstate = STOPBETWEENFLOORS;
+		}
+		else {
+			elev_set_door_open_lamp(0);
+			currentstate = IDLE;
+			
+		}
+		break;
+
+	default:
+		break;
+	}
+	
+void fsm_event_door_closed(){
+	switch(currentstate){
+	case DOOROPEN:
+		elev_set_door_open_lamp(0);
+		direction = queue_get_direction(previous_direction, currentfloor);
+		elev_set_motor_direction(direction);
+		previous_direction = direction;
+
+		if(direction!=DIRN_STOP){
+			currentstate = MOVING;
+		} 
+		else {
+			currentstate = IDLE;
+		}		
+		break;
+
+	default:
+		break;
+	}		
+}
+	
+	
+}
+}
