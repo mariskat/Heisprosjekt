6b2992721069c139b6a668ce2a950b3508c46410
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 13:29:39 +0100

    Update fsm.c

--- fsm.c
+++ fsm.c
@@ -5,6 +5,15 @@
 
 #include <stdio.h>
 
+/*Included from elev.h:
+
+Sets the motor direction of the elevator: elev_motor_direction_t is enum: DIRN_DOWN=-1, DIRN_STOP=0, DIRN_UP=1
+void elev_set_motor_direction(elev_motor_direction_t dirn);
+
+Get floor sensor signal: Not on floor = -1, On floor = 0-3 (ground fl.- top fl.)
+int elev_get_floor_sensor_signal(void);
+*/
+
 typedef enum{
 	INITIALIZE,
 	IDLE,
