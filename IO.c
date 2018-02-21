355df0d8ffc9f365b49338600d962762fb341442
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 13:26:31 +0100

    Update and rename I/O.c to IO.c

--- IO.c
+++ IO.c
@@ -15,7 +15,8 @@
 //Floor indicator
 //void elev_set_floor_indicator(int floor);
 
-
+//elev_button_type_t er en enum: BUTTON_CALL_UP=0, BUTTON_CALL_DOWN=1, BUTTON_COMMAND=2
+int elev_get_button_signal(elev_button_type_t button, int floor);
 
 void clear_lights_at_floor(floor){
   elev_set_button_lamp(BUTTON_CALL_DOWN, floor, 0);
