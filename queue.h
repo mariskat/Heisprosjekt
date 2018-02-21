e91865dc3b5e925e9ba784077781043443bf1b01
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 14:44:32 +0100

    Update queue.h

--- queue.h
+++ queue.h
@@ -14,19 +14,17 @@ int orders[][];
 //Funksjoner:
 
 //elev_button_type_t er en enum: BUTTON_CALL_UP=0, BUTTON_CALL_DOWN=1, BUTTON_COMMAND=2
-void add_to_queue(floor,elev_button_type_t buttons);
+void add_to_queue(int floor,elev_button_type_t buttons);
 
 bool check_floor_orders();
 
-void clear_orders_at_floor(int floor);
+void clear_orders_at_floor( int floor);
 
 void clear_all_orders();
 
-bool get_new_direction(int previous_floor, int previous_direction);
+int get_new_direction(int previous_direction, int previous_floor);
 
-
-int stop_queue(int floor, );
-
-
+//Sets the motor direction of the elevator: elev_motor_direction_t is enum: DIRN_DOWN=-1, DIRN_STOP=0, DIRN_UP=1
+int stop_queue(int floor, elev_button_type_t buttons);
 
 
