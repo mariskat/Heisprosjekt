8bbc2785167817c917d92651f7dd1f1510362828
laksiya <36662731+laksiya@users.noreply.github.com>
2018-02-21 13:24:46 +0100

    Update main.c

--- main.c
+++ main.c
@@ -8,9 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main() {
-    // Initialize hardware
+int last_stop_button;
+int last_floor_visited = -9;
 
+int main() {
+    //  INITIALIZING HARDWARE
     if (!elev_init()) {
         printf("Unable to initialize elevator hardware!\n");
         return 1;
@@ -20,6 +22,7 @@ int main() {
 
     elev_set_motor_direction(DIRN_UP);
 
+    // CONTINOUS LOOP
     while (1) {
         // Change direction when we reach top/bottom floor
         if (elev_get_floor_sensor_signal() == N_FLOORS - 1) {
@@ -29,22 +32,40 @@ int main() {
         }
 
         // Stop elevator and exit program if the stop button is pressed
-        if (elev_get_stop_signal()) {
-            elev_set_motor_direction(DIRN_STOP);
-            break;
+        int stopbtn = elev_get_stop_signal();
+        if(stopbtn != last_stop_button){
+            if(stopbtn){
+                fsm_event_stop_pressed();
+            } else {
+                fsm_event_stop_released();
+            }
+        }
+        last_stop_button = stopbtn;
           
         
-        //overordnet buttonsfunksjon
+        //Elevator order
+        int buttonfloor;
+        int buttontype;
+        for (buttonfloor = 0; buttonfloor < N_FLOORS; buttonfloor++){
+        	for(buttontype = 0; buttontype < N_BUTTONS; buttontype++){
+            	int test = elev_get_button_signal(buttontype,buttonfloor);
+            	if (test){
+            		fsm_event_order_button_pressed(buttontype, buttonfloor);
+					if (buttonfloor == last_floor_visited){
+						fsm_event_arrived_at_floor(last_floor_visited);
+					}
+				}
+        	}
+        }
         
         //Arrived@Floor
-            //
-            int current_floor = elev_get_floor_sensor_signal(void);
-            elev_set_floor_indicator(current_floor);
-            if (check_floor_orders()){
-                elev_set_motor_direction(DIRN_STOP);
-                clear_orders_at_floor();
-                elev_set_door_open_lamp(1);                 //Wait 3 seconds
-                elev_set_motor_direction(queue.get_new_direction());  //Reevaluvates most efficient direction
+            int floor = elev_get_floor_sensor_signal();
+            if(floor != last_floor_visited  &&  floor != -1){           //elev_get_floor_sensor_signal returns -1 while not at a floor
+                fsm_event_arrived_at_floor(floor);
+            }
+            last_floor_visited = floor;
+        
+        
             }
             
             
