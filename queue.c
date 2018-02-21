c28bd37142b4b692b1976ef3780726101af67926
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-21 14:58:09 +0100

    Update queue.c

--- queue.c
+++ queue.c
@@ -11,24 +11,27 @@ int orders[4][3]={
 //Funksjoner:
 
 //Given floor and buttontype, this function sets the following button to 1 in queue.
-void add_to_queue(int floor, elev_button_type_t button){
-  printf("Running add_to_queue");
+void queue_add_to(int floor, elev_button_type_t button){
   orders[floor][button]={1};
 };    
 
 //Sums the orders of the floor, returns 0 if no orders, 1 if else.
-bool check_floor_orders(int floor){
-  printf("Running check_floor_orders");
+bool queue_check_floor_orders(int floor){
   return (orders[floor][0]+orders[floor][1]+orders[floor][2]);
 };
 
 //Clears floor orders
-void clear_orders_at_floor(int floor){
-  printf("Running clear_orders_at_floor");
+void queue_clear_orders_at_floor(int floor){
   orders[floor]={0,0,0};
-  printf("Done clear_orders_at_floor");
 };
 
-elev_motor_direction_t get_new_direction(){
+elev_motor_direction_t queue_get_new_direction(){
   TILSTANDSMASKIN????
 };
+
+
+void queue_clear_all_orders(){
+ 
+  ;
+
+int queue_stop(int floor, elev_button_type_t buttons);
