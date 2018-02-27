f253f1b6bf71ce4326bf55bfdee0b9d01d96fcb4
mariskat <36444161+mariskat@users.noreply.github.com>
2018-02-27 10:30:39 +0100

    Update timer.h

--- timer.h
+++ timer.h
@@ -2,9 +2,11 @@
 #include "elev.h"
 #include "io.h"
 
+#include <stdbool.h>
 #include <assert.h>
 #include <stdlib.h>
 #include <time.h>
+
 
 static clock_t start_time = -1;                 //Static - da bevarer den verdien ute av scope. Døren er i utgangspunktet lukket: -1. 
 
@@ -19,8 +21,3 @@ void timer_stop(void);
 //Returns true if 3 seconds (or more) has passed. If not - or door is closed - returns false. 
 bool timer_timeIsUp(void);
 
-
-
-
-
-#endif /*timer.h*/
