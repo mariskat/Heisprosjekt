d6c18aa76578757df47bbd5d733fe1bfa5766741
mariskat <36444161+mariskat@users.noreply.github.com>
2018-03-06 22:34:54 +0000

    Update timer.c

--- timer.c
+++ timer.c
@@ -1,25 +1,35 @@
 
 
 //Start timer
-void timer_start(void){                          //Start timer if the door is open
+void 
+timer_start(void)                               //Start timer if the door is open
+{                          
     start_time = time(NULL); 
 }
 
 // Stop timer
-void timer_stop(void){
-    start_time = -1;                             //Return -1 for failure
-  
+void 
+timer_stop(void)                                //Return -1 for failure
+{
+    start_time = -1;                             
+}
   
 // Check if time is up (3 sec)
-bool timer_ timeIsUp(void){
+bool 
+timer_ timeIsUp(void)
+{
   
-  if (start_time==-1){                           //Return false if the door is closed
+  if (start_time==-1)                            //Return false if the door is closed
+  {                           
        return false;
   }
   
-  if (time(NULL) - start_time > 3){              //Is the difference between the time right now and the starttime more than 3 sec? 
+  if (time(NULL) - start_time > 3)                //Is the difference between the time right now and the starttime more than 3 sec? 
+  {              
         return true;
-  } else {
+  } 
+    else 
+  {
          return false;
   }
 }
