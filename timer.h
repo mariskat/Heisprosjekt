3f6cf4154a2b4a84f58b4cda96bddd569d81a9a9
mariskat <36444161+mariskat@users.noreply.github.com>
2018-03-06 22:35:31 +0000

    Update timer.h

--- timer.h
+++ timer.h
@@ -8,16 +8,20 @@
 #include <time.h>
 
 
-static clock_t start_time = -1;                 //Static - da bevarer den verdien ute av scope. Døren er i utgangspunktet lukket: -1. 
+static 
+  clock_t start_time = -1;                 //Static - da bevarer den verdien ute av scope. Døren er i utgangspunktet lukket: -1. 
 
 //Using time() in seconds to set start_time
-void timer_start(void);
+void 
+timer_start(void);
 
 
 //Failing start_time (=-1)
-void timer_stop(void);
+void 
+timer_stop(void);
 
 
 //Returns true if 3 seconds (or more) has passed. If not - or door is closed - returns false. 
-bool timer_timeIsUp(void);
+bool 
+timer_timeIsUp(void);
 
